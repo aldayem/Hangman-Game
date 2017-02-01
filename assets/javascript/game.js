52c1273067dd8e18d2d34545c64528ec172cc07c
Ahmad Aldayem <ahmadaldayem@ahmads-mbp.attlocal.net>
2017-01-31 23:17:20 -0800

    add new file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,28 +9,29 @@ var lsgwoerter = [
 ]
 var random = Math.floor((Math.random()*(lsgwoerter.length-1))); 
 
-var lsgwort = lsgwoerter[random];
+var lsgwort = lsgwoerter[random]; // the word to guess will be chosen from the array above
 var ratewort = new Array(lsgwort.length);
 var fehler = 0;
 
-
+// every letter in the word is symbolized by an underscore in the guessfield
 for (var i = 0; i < ratewort.length; i++){
   ratewort[i] = "_ ";
 }
 
-
+// prints the guessfield
 function printRatewort(){
   for (var i = 0; i < ratewort.length; i++){
-  var ratefeld = document.getElementById("ratefeld");
+  var contaner = document.getElementById("contaner");
   var buchstabe = document.createTextNode(ratewort[i]);
-  ratefeld.appendChild(buchstabe);
+  contaner.appendChild(buchstabe);
   }
 }
 
+//checks if the the letter provided by the user matches one or more of the letters in the word
 var pruefeZeichen = function(){
   var f = document.rateformular; 
   var b = f.elements["ratezeichen"]; 
-  var zeichen = b.value; 
+  var zeichen = b.value; // the letter provided by the user
   zeichen = zeichen.toUpperCase();
   for (var i = 0; i < lsgwort.length; i++){
     if(lsgwort[i] === zeichen){
@@ -40,12 +41,12 @@ var pruefeZeichen = function(){
   b.value = "";
   }
   
-  
-  var ratefeld = document.getElementById("ratefeld");
-  ratefeld.innerHTML=""; 
+  //deletes the guessfield and replaces it with the new one
+  var contaner = document.getElementById("contaner");
+  contaner.innerHTML=""; 
   printRatewort();
-  
-  
+
+  // if a guessed letter is not in the word, the letter will be put on the "wrong letters"-list and hangman grows
   if(!treffer){
     var gerateneBuchstaben = document.getElementById("gerateneBuchstaben");
     var buchstabe = document.createTextNode(" " + zeichen);
@@ -55,7 +56,7 @@ var pruefeZeichen = function(){
     hangman.src = "http://www.writteninpencil.de/Projekte/Hangman/hangman" + fehler + ".png";
   }
   
-  
+  //checks if all letters have been found
   var fertig = true;
   for (var i = 0; i < ratewort.length; i++){
     if(ratewort[i] === "_ "){
@@ -66,7 +67,7 @@ var pruefeZeichen = function(){
     window.alert("You win!");
   }
   
-  
+  //once you got six wrong letters, you lose
   if(fehler === 6){
     window.alert("Uh...I guess you're dead now.");
   }
